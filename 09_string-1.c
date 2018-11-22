fa00365a67a84b00e59d995248bde1530555eca2
soyoungna <hos05360@naver.com>
2018-11-22 16:15:55 +0900

    09_string-5

--- 09_string-1.c
+++ 09_string-1.c
@@ -4,18 +4,29 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[100] = "programming course";
-	char dst[100];
-	int i=0;
-	
-	while(src[i]!='\0')
-	{
-		dst[i]=src[i];
-		i++;
+	FILE *fp1=NULL;
+	FILE *fp2=NULL;
+    char input;
+    char original[100];
+    char copy[100];
+    int i=0;
+    
+    printf("original file : ");
+    scanf("%s",original);
+    printf("copy file : ");
+    scanf("%s", copy);
+    
+    fp1= fopen(original,"r");
+    fp2= fopen(copy,"w");
+    
+    while((input = fgetc(fp1))!=EOF)
+    {
+    	fputc(input, fp2);
+    	i++;
 	}
 	
-	dst[i]='\0';
-	
-	printf("dst : %s\n",dst);
+	printf("copy succeed~(%i Bytes copied)\n",i);
+	fclose(fp1);
+	fclose(fp2);
 	return 0;
 }
